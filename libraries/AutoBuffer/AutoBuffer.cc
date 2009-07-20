ded9159a3efb50ae0ae3cabceb7d5e63610e21a8
John Bridgman <johnfb@arlut.utexas.edu>
2009-07-20 16:18:28 -0500

    Added a set of hash functions and some small modifications to AutoBuffer.

--- libraries/AutoBuffer/AutoBuffer.cc
+++ libraries/AutoBuffer/AutoBuffer.cc
@@ -10,65 +10,69 @@
 typedef unsigned long ulong;
 typedef char* caddr_t;
 
+AutoBuffer::AutoBuffer()
+    : buffer(0), size(0) {
+}
+
 AutoBuffer::AutoBuffer(ulong initialsize)
-	: buffer(0), size(0) {
-	ChangeSize(initialsize);
+    : buffer(0), size(0) {
+    ChangeSize(initialsize);
 }
 
 AutoBuffer::AutoBuffer(const AutoBuffer& other) : buffer(0), size(0) {
-	ulong othersize = other.GetSize();
-	ChangeSize(othersize);
-	other.Get(buffer, othersize);
+    ulong othersize = other.GetSize();
+    ChangeSize(othersize);
+    other.Get(buffer, othersize);
 }
 
 AutoBuffer::AutoBuffer(void* other, ulong othersize) : buffer(0), size(0) {
-	ChangeSize(othersize);
-	Put(other, othersize);
+    ChangeSize(othersize);
+    Put(other, othersize);
 }
 
 AutoBuffer::~AutoBuffer() {
-	ChangeSize(0);
+    ChangeSize(0);
 }
 
 
 AutoBuffer& AutoBuffer::operator=(const AutoBuffer& other) {
-	if (size < other.GetSize()) {
-		ChangeSize(other.GetSize());
-	}
-	other.Get(buffer, other.GetSize());
+    if (size < other.GetSize()) {
+        ChangeSize(other.GetSize());
+    }
+    other.Get(buffer, other.GetSize());
 }
 
 void* AutoBuffer::GetBuffer(ulong offset) {
-	return (caddr_t)buffer + offset;
+    return (caddr_t)buffer + offset;
 }
 
 void AutoBuffer::ChangeSize(ulong newsize) {
-	void *newbuffer = realloc(buffer, newsize);
-	if (0 == newbuffer && newsize != 0) {
-		throw std::bad_alloc();
-	}
-	buffer = newbuffer;
-	size = newsize;
+    void *newbuffer = realloc(buffer, newsize);
+    if (0 == newbuffer && newsize != 0) {
+        throw std::bad_alloc();
+    }
+    buffer = newbuffer;
+    size = newsize;
 }
 
 
 void AutoBuffer::Put(const void* other, const ulong othersize, const ulong offset) {
-	if (offset + othersize > size) {
-		ChangeSize(offset + othersize);
-	}
-	caddr_t spot = (caddr_t)buffer + offset;
-	memcpy(buffer, other, othersize);
+    if (offset + othersize > size) {
+        ChangeSize(offset + othersize);
+    }
+    caddr_t spot = (caddr_t)buffer + offset;
+    memcpy(buffer, other, othersize);
 }
 
 ulong AutoBuffer::Get(void* other, const ulong othersize, const ulong offset) const {
-	ulong copysize = othersize;
-	if (offset >= size) {
-		return 0;
-	} else if (offset + othersize >= size) {
-		copysize = size - offset;
-	}
-	caddr_t spot = (caddr_t)buffer + offset;
-	memcpy(other, spot, copysize);
-	return copysize;
+    ulong copysize = othersize;
+    if (offset >= size) {
+        return 0;
+    } else if (offset + othersize >= size) {
+        copysize = size - offset;
+    }
+    caddr_t spot = (caddr_t)buffer + offset;
+    memcpy(other, spot, copysize);
+    return copysize;
 }
 
--- libraries/AutoBuffer/AutoBuffer.h
+++ libraries/AutoBuffer/AutoBuffer.h
@@ -19,69 +19,74 @@
  */
 class AutoBuffer {
 public:
-	typedef unsigned long ulong;
-	AutoBuffer(ulong initialsize);
-	AutoBuffer(const AutoBuffer& other);
-	AutoBuffer(void* other, ulong othersize);
-	~AutoBuffer();
-	
-	AutoBuffer& operator=(const AutoBuffer& other);
-	/**
-	 * \return a pointer to the buffer.
-	 */
-	void* GetBuffer() { return buffer; }
+    typedef unsigned long ulong;
+    /// Initial size of 0
+    AutoBuffer();
+    /// Start with initialsize
+    AutoBuffer(ulong initialsize);
+    /// Copy other buffer
+    AutoBuffer(const AutoBuffer& other);
+    /// Copy a pointer
+    AutoBuffer(void* other, ulong othersize);
+    ~AutoBuffer();
+    /// Copy contents of other buffer into us destroying us 
+    AutoBuffer& operator=(const AutoBuffer& other);
+    /**
+     * \return a pointer to the buffer.
+     */
+    void* GetBuffer() { return buffer; }
 
-	void* GetBuffer(ulong offset);
+    void* GetBuffer(ulong offset);
 
-	operator void*() { return buffer; }
+    operator void*() { return buffer; }
 
-	/**
-	 * \return the current size of the buffer.
-	 */
-	ulong GetSize() const { return size; }
+    /**
+     * \return the current size of the buffer.
+     */
+    ulong GetSize() const { return size; }
 
-	/**
-	 * Change the size of this buffer. If newsize is larger than
-	 * current size then the buffer will be extended. The data
-	 * already in buffer will remain unchanged. See realloc.
-	 * If newsize is smaller then the buffer will be truncated.
-	 * \param newsize the new size in bytes for this buffer
-	 */
-	void ChangeSize(ulong newsize);
+    /**
+     * Change the size of this buffer. If newsize is larger than
+     * current size then the buffer will be extended. The data
+     * already in buffer will remain unchanged. See realloc.
+     * If newsize is smaller then the buffer will be truncated.
+     * \param newsize the new size in bytes for this buffer
+     */
+    void ChangeSize(ulong newsize);
 
-	/**
-	 * Copy othersize bytes from other into this buffer starting at
-	 * offset. If there is not enough space this buffer will try to expand.
-	 * \param other the other memory buffer
-	 * \param othersize the number of bytes to copy
-	 * \param offset the offset into us to start copying at
-	 */
-	void Put(const void* other, const ulong othersize, const ulong offset=0);
+    /**
+     * Copy othersize bytes from other into this buffer starting at
+     * offset. If there is not enough space this buffer will try to expand.
+     * \param other the other memory buffer
+     * \param othersize the number of bytes to copy
+     * \param offset the offset into us to start copying at
+     */
+    void Put(const void* other, const ulong othersize, const ulong offset=0);
 
-	template <class T>
-	void Put(const ulong offset, const T* other, ulong count = 1) {
-		Put(other, sizeof(T) * count, offset);
-	}
+    template <class T>
+    void Put(const ulong offset, const T* other, ulong count = 1) {
+        Put(other, sizeof(T) * count, offset);
+    }
 
-	/**
-	 * Copy up to othersize bytes into other starting at offset.
-	 * If othersize+offset is larger than our size then only copy up to the
-	 * end of us. Returns the number of bytes actually copied.
-	 * \param other the other buffer to copy into
-	 * \param othersize the number of bytes to copy into other
-	 * \param offset where to start copying in us
-	 * \return the number of bytes actually copied
-	 */
-	ulong Get(void* other, const ulong othersize, const ulong offset=0) const;
+    /**
+     * Copy up to othersize bytes into other starting at offset.
+     * If othersize+offset is larger than our size then only copy up to the
+     * end of us. Returns the number of bytes actually copied.
+     * \param other the other buffer to copy into
+     * \param othersize the number of bytes to copy into other
+     * \param offset where to start copying in us
+     * \return the number of bytes actually copied
+     */
+    ulong Get(void* other, const ulong othersize, const ulong offset=0) const;
 
-	template <class T>
-	ulong Get(const ulong offset, T* other, ulong count = 1) const {
-		return Get(other, sizeof(T) * count, offset);
-	}
+    template <class T>
+    ulong Get(const ulong offset, T* other, ulong count = 1) const {
+        return Get(other, sizeof(T) * count, offset);
+    }
 
 private:
-	void* buffer;
-	ulong size;
+    void* buffer;
+    ulong size;
 };
 
 #endif
