fb557915bdb493602a27eaa309bb2ccf88ac4951
John Bridgman <johnfb@arlut.utexas.edu>
2010-02-08 10:55:23 -0600

    Adding a simple logical clock as described in the paper by Lamport titled 'Time, clocks, and the ordering of events in a distributed system'

---
+++ libraries/LogicalClock/LogicalClock.h
@@ -0,0 +1,52 @@
+//=============================================================================
+//	Computational Process Networks class library
+//	Copyright (C) 1997-2006  Gregory E. Allen and The University of Texas
+//
+//	This library is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Library General Public License as published
+//	by the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This library is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//	Library General Public License for more details.
+//
+//	The GNU Public License is available in the file LICENSE, or you
+//	can write to the Free Software Foundation, Inc., 59 Temple Place -
+//	Suite 330, Boston, MA 02111-1307, USA, or you can find it on the
+//	World Wide Web at http://www.fsf.org.
+//=============================================================================
+/** \file
+ * \author John Bridgman
+ */
+#ifndef LOGICALCLOCK_H
+#define LOGICALCLOCK_H
+
+/** \brief A simple implementation of Lamport's logical clock.
+ */
+class LogicalClock {
+public:
+    typedef unsigned long long Clock_t;
+
+    LogicalClock();
+
+    /** \return the current value of the logical clock */
+    Clock_t Get() const { return value; }
+
+    /** \brief Increment the logical clock
+     * \return the old value of the logical clock
+     */
+    Clock_t Tick() { return value++; }
+
+    /** \brief Update the logical clock with the
+     * given value obtained from another logical clock.
+     * \return the old value
+     */
+    Clock_t Update(Clock_t v);
+
+private:
+    unsigned long long value;
+};
+
+#endif
