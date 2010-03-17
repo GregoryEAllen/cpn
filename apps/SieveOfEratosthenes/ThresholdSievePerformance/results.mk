75eeef8a8fde7bdb1219bfe7706938c010168d0e
John Bridgman <johnfb@arlut.utexas.edu>
2010-03-17 09:13:58 -0500

    Stuff to generate graphs of ThresholdSieve performance

---
+++ apps/SieveOfEratosthenes/ThresholdSievePerformance/results.mk
@@ -0,0 +1,115 @@
+TARGETS = result.0 result.1 result.2 result.3 result.4 result.5 result.6 result.7 result.8 result.9 result.10 result.11 result.12 result.13 result.14 result.15 result.16 result.17 result.18 result.19 result.20 result.21 result.22 result.23 result.24 result.25 result.26 result.27 result.28 result.29 result.30 result.31 result.32 result.33 result.34 result.35 result.36 result.37
+result.0:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 0 -z 0 -f result.0 -s
+
+result.1:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 0 -z 0 -f result.1 -s
+
+result.2:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 0 -z 0 -f result.2 -s
+
+result.3:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 0 -z 0 -f result.3 -s
+
+result.4:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 5 -z 0 -f result.4 -s
+
+result.5:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 5 -z 0 -f result.5 -s
+
+result.6:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 5 -z 0 -f result.6 -s
+
+result.7:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 1,0,0 -i 5 -w 5 -z 0 -f result.7 -s
+
+result.8:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.8 -s
+
+result.9:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.9 -s
+
+result.10:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.10 -s
+
+result.11:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.11 -s
+
+result.12:
+	$(PROG) -m 10000000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.12 -s
+
+result.13:
+	$(PROG) -m 100000000 -q 5000 -t 2500 -p 5,0,0 -i 5 -w 5 -z 0 -f result.13 -s
+
+result.14:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.14 -s
+
+result.15:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.15 -s
+
+result.16:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.16 -s
+
+result.17:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.17 -s
+
+result.18:
+	$(PROG) -m 10000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.18 -s
+
+result.19:
+	$(PROG) -m 100000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 0 -f result.19 -s
+
+result.20:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.20 -s
+
+result.21:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.21 -s
+
+result.22:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.22 -s
+
+result.23:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.23 -s
+
+result.24:
+	$(PROG) -m 10000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.24 -s
+
+result.25:
+	$(PROG) -m 100000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 1 -f result.25 -s
+
+result.26:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.26 -s
+
+result.27:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.27 -s
+
+result.28:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.28 -s
+
+result.29:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.29 -s
+
+result.30:
+	$(PROG) -m 10000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.30 -s
+
+result.31:
+	$(PROG) -m 100000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 2 -f result.31 -s
+
+result.32:
+	$(PROG) -m 1000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.32 -s
+
+result.33:
+	$(PROG) -m 10000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.33 -s
+
+result.34:
+	$(PROG) -m 100000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.34 -s
+
+result.35:
+	$(PROG) -m 1000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.35 -s
+
+result.36:
+	$(PROG) -m 10000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.36 -s
+
+result.37:
+	$(PROG) -m 100000000 -q 5000 -t 2500 -p 0.5,0.5,0.5 -i 5 -w 5 -z 3 -f result.37 -s
+
