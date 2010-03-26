4a9e8d0d0ccbed314099afe13322545e09ffaf6a
John Bridgman <johnfb@arlut.utexas.edu>
2010-03-26 13:16:38 -0500

    Modifications to MirrorBufferSet to use POSIX shared memory objects on linux

--- libraries/MirrorBufferSet/MirrorBufferSet.cc
+++ libraries/MirrorBufferSet/MirrorBufferSet.cc
@@ -1,7 +1,7 @@
 //=============================================================================
-//	$Id: MirrorBufferSet.cc,v 1.1 2006/06/12 20:21:44 gallen Exp $
+//  $Id: MirrorBufferSet.cc,v 1.1 2006/06/12 20:21:44 gallen Exp $
 //-----------------------------------------------------------------------------
-//	A set of "mirrored" buffers
+//  A set of "mirrored" buffers
 //=============================================================================
 
 
@@ -11,28 +11,31 @@
 
 #include <unistd.h>
 
-#if defined(_POSIX_SHARED_MEMORY_OBJECTS) && !defined(OS_LINUX) && !defined(OS_DARWIN)
-	#define MBS_USE_POSIX_SHM 1
-	#define _POSIX_C_SOURCE 199309
-	#include <string.h>
+#if defined(_POSIX_SHARED_MEMORY_OBJECTS) && !defined(OS_DARWIN)
+    #define MBS_USE_POSIX_SHM 1
+#if !defined(OS_LINUX)
+    #define _POSIX_C_SOURCE 199309
+#endif
+    #include <string.h>
+    #include <errno.h>
 #else
-	#define MBS_USE_POSIX_SHM 0
-//	#warning POSIX shared memory objects not supported
+    #define MBS_USE_POSIX_SHM 0
+//  #warning POSIX shared memory objects not supported
 #endif
 
 //-----------------------------------------------------------------------------
 int MirrorBufferSet::Supported(void)
 //-----------------------------------------------------------------------------
 {
-	#if !MirrorBufferSet_SUPPORTED
-		return eNotSupported;
-	#endif
+    #if !MirrorBufferSet_SUPPORTED
+        return eNotSupported;
+    #endif
 
-	#if MBS_USE_POSIX_SHM
-		return eSupportedPosixShm;
-	#else
-		return eSupportedTmpFile;
-	#endif
+    #if MBS_USE_POSIX_SHM
+        return eSupportedPosixShm;
+    #else
+        return eSupportedTmpFile;
+    #endif
 }
 
 
@@ -49,13 +52,13 @@ int MirrorBufferSet::Supported(void)
 
 
 #ifndef MAP_FAILED
-	// not defined in AIX, HPUX
-	#define MAP_FAILED		((void*)-1)
+    // not defined in AIX, HPUX
+    #define MAP_FAILED      ((void*)-1)
 #endif
 
 #ifndef MAP_NORESERVE
-	// not defined in AIX, IRIX, LinuxPPC
-	#define MAP_NORESERVE	(0)
+    // not defined in AIX, IRIX, LinuxPPC
+    #define MAP_NORESERVE   (0)
 #endif
 
 // on AIX and HPUX, this compiles, but give a runtime error from mmap()
@@ -63,7 +66,7 @@ int MirrorBufferSet::Supported(void)
 
 
 //-----------------------------------------------------------------------------
-//	flags and definitions for debugging printouts
+//  flags and definitions for debugging printouts
 //-----------------------------------------------------------------------------
 #ifndef MirrorBufferSet_VERBOSITY
 #define MirrorBufferSet_VERBOSITY 0
@@ -85,99 +88,99 @@ int MirrorBufferSet::Supported(void)
 //-----------------------------------------------------------------------------
 MirrorBufferSet::MirrorBufferSet(ulong bufferSz, ulong mirrorSz, int nBuffers)
 //-----------------------------------------------------------------------------
-:	bufferBase(0),
-	bufferSize(bufferSz),
-	mirrorSize(mirrorSz),
-	numBuffers(nBuffers)
+:   bufferBase(0),
+    bufferSize(bufferSz),
+    mirrorSize(mirrorSz),
+    numBuffers(nBuffers)
 {
-	fileName[0] = 0;
-	ulong pageSize = PageSize();
+    fileName[0] = 0;
+    ulong pageSize = PageSize();
 
-	// if bufferSize or mirrorSize are not a multiple of pageSize, round them up
-	if (bufferSize % pageSize) {
-		bufferSize += pageSize - (bufferSize % pageSize);
-	}
-	if (mirrorSize % pageSize) {
-		mirrorSize += pageSize - (mirrorSize % pageSize);
-	}
+    // if bufferSize or mirrorSize are not a multiple of pageSize, round them up
+    if (bufferSize % pageSize) {
+        bufferSize += pageSize - (bufferSize % pageSize);
+    }
+    if (mirrorSize % pageSize) {
+        mirrorSize += pageSize - (mirrorSize % pageSize);
+    }
 
 #if 1
-	// do a sanity check for our math
-	if ( bufferSize % pageSize)
-		fprintf(stderr, "### Error: bufferSize = %lu is not a multiple of pageSize = %lu\n",
-			bufferSize, pageSize);
-	if ( mirrorSize % pageSize)
-		fprintf(stderr, "### Error: mirrorSize = %lu is not a multiple of pageSize = %lu\n",
-			mirrorSize, pageSize);
+    // do a sanity check for our math
+    if ( bufferSize % pageSize)
+        fprintf(stderr, "### Error: bufferSize = %lu is not a multiple of pageSize = %lu\n",
+            bufferSize, pageSize);
+    if ( mirrorSize % pageSize)
+        fprintf(stderr, "### Error: mirrorSize = %lu is not a multiple of pageSize = %lu\n",
+            mirrorSize, pageSize);
 #endif
 
-	int fd = GetFileDescriptor();
+    int fd = GetFileDescriptor();
 
-	// make the file large enough
-	if ( ftruncate(fd, bufferSize*numBuffers) ) {
-		perror("ftruncate");
-		close(fd);
-		return;
-	}
+    // make the file large enough
+    if ( ftruncate(fd, bufferSize*numBuffers) ) {
+        perror("ftruncate");
+        close(fd);
+        return;
+    }
 
-	// map the entire buffer space, so there will be enough space reserved
-	ulong numBytes = (bufferSize+mirrorSize) * numBuffers;
-	fprintf1((stderr,"MirrorBufferSet: reserving %d bytes for mapping ", numBytes));
-	caddr_t baseAddr = (caddr_t) mmap(0, numBytes, PROT_READ|PROT_WRITE, 
-			MAP_SHARED|MAP_NORESERVE, fd, 0);
-	if (baseAddr == MAP_FAILED) {
-		fprintf1((stderr,"failed\n"));
-		perror("mmap");
-		return;
-	}
-	bufferBase = baseAddr;
-	fprintf1((stderr,"[0x%08X - 0x%08X)\n", baseAddr, baseAddr+numBytes));
-	fprintf1((stderr,"MirrorBufferSet: file \"%s\" is %d bytes\n",
-		fileName, bufferSize*numBuffers));
+    // map the entire buffer space, so there will be enough space reserved
+    ulong numBytes = (bufferSize+mirrorSize) * numBuffers;
+    fprintf1((stderr,"MirrorBufferSet: reserving %d bytes for mapping ", numBytes));
+    caddr_t baseAddr = (caddr_t) mmap(0, numBytes, PROT_READ|PROT_WRITE, 
+            MAP_SHARED|MAP_NORESERVE, fd, 0);
+    if (baseAddr == MAP_FAILED) {
+        fprintf1((stderr,"failed\n"));
+        perror("mmap");
+        return;
+    }
+    bufferBase = baseAddr;
+    fprintf1((stderr,"[0x%08X - 0x%08X)\n", baseAddr, baseAddr+numBytes));
+    fprintf1((stderr,"MirrorBufferSet: file \"%s\" is %d bytes\n",
+        fileName, bufferSize*numBuffers));
 
-	// now map each of the individual buffers twice
-	for (int buf=0; buf<numBuffers; buf++) {
-		// once for the buffer
-		caddr_t theAddr		= baseAddr+buf*(bufferSize+mirrorSize);
-		size_t  theSize		= bufferSize;
-		size_t  theOffset	= buf*bufferSize;
-		fprintf2((stderr,"mapping 0x%06lX bytes @ offset 0x%06lX ", theSize, theOffset));
-		caddr_t addr = (caddr_t) mmap(theAddr, theSize, PROT_READ|PROT_WRITE,
-			MAP_SHARED|MAP_FIXED|MAP_NORESERVE, fd, theOffset);
-		if (addr == MAP_FAILED) {
-			fprintf2((stderr,"failed\n"));
-			perror("mmap");
-			return;
-		}
-		fprintf2((stderr,"to [0x%08X - 0x%08X)\n", theAddr, theAddr+theSize));
-		if (addr!=theAddr) {
-			fprintf(stderr, "### Error: mapped to 0x%p instead of 0x%p\n",
-				addr, theAddr );
-			return;
-		}
+    // now map each of the individual buffers twice
+    for (int buf=0; buf<numBuffers; buf++) {
+        // once for the buffer
+        caddr_t theAddr     = baseAddr+buf*(bufferSize+mirrorSize);
+        size_t  theSize     = bufferSize;
+        size_t  theOffset   = buf*bufferSize;
+        fprintf2((stderr,"mapping 0x%06lX bytes @ offset 0x%06lX ", theSize, theOffset));
+        caddr_t addr = (caddr_t) mmap(theAddr, theSize, PROT_READ|PROT_WRITE,
+            MAP_SHARED|MAP_FIXED|MAP_NORESERVE, fd, theOffset);
+        if (addr == MAP_FAILED) {
+            fprintf2((stderr,"failed\n"));
+            perror("mmap");
+            return;
+        }
+        fprintf2((stderr,"to [0x%08X - 0x%08X)\n", theAddr, theAddr+theSize));
+        if (addr!=theAddr) {
+            fprintf(stderr, "### Error: mapped to 0x%p instead of 0x%p\n",
+                addr, theAddr );
+            return;
+        }
 
-		// and again for the mirror
-		theAddr		+=bufferSize;
-		theSize		= mirrorSize;
-		fprintf2((stderr,"mapping 0x%06lX bytes @ offset 0x%06lX ", theSize, theOffset));
-		addr = (caddr_t) mmap(theAddr, theSize, PROT_READ|PROT_WRITE,
-			MAP_SHARED|MAP_FIXED|MAP_NORESERVE, fd, theOffset);
-		if (addr == MAP_FAILED) {
-			fprintf2((stderr,"failed\n"));
-			perror("mmap");
-			return;
-		}
-		fprintf2((stderr,"to [0x%08X - 0x%08X)\n", theAddr, theAddr+theSize));
-		if (addr!=theAddr) {
-			fprintf(stderr, "### Error: mapped to 0x%p instead of 0x%p\n", addr, theAddr );
-			return;
-		}
-	}
+        // and again for the mirror
+        theAddr     +=bufferSize;
+        theSize     = mirrorSize;
+        fprintf2((stderr,"mapping 0x%06lX bytes @ offset 0x%06lX ", theSize, theOffset));
+        addr = (caddr_t) mmap(theAddr, theSize, PROT_READ|PROT_WRITE,
+            MAP_SHARED|MAP_FIXED|MAP_NORESERVE, fd, theOffset);
+        if (addr == MAP_FAILED) {
+            fprintf2((stderr,"failed\n"));
+            perror("mmap");
+            return;
+        }
+        fprintf2((stderr,"to [0x%08X - 0x%08X)\n", theAddr, theAddr+theSize));
+        if (addr!=theAddr) {
+            fprintf(stderr, "### Error: mapped to 0x%p instead of 0x%p\n", addr, theAddr );
+            return;
+        }
+    }
 
-	// close the file
-	if ( close(fd) ) {
-		perror("close");
-	}
+    // close the file
+    if ( close(fd) ) {
+        perror("close");
+    }
 }
 
 
@@ -185,34 +188,45 @@ MirrorBufferSet::MirrorBufferSet(ulong bufferSz, ulong mirrorSz, int nBuffers)
 int MirrorBufferSet::GetFileDescriptor(void)
 //-----------------------------------------------------------------------------
 {
-	// get a temporary file name
-#ifdef OS_LINUX
-	sprintf(fileName, "/tmp/MirrorBufferSet-XXXXXX");
-	return mkstemp(fileName);
-#endif
-	tmpnam(fileName);
-	fprintf2((stderr,"tmpnam returned \"%s\"\n",fileName));
+    // get a temporary file name
 #if MBS_USE_POSIX_SHM
-	#if !defined(OS_IRIX) && !defined(OS_HPUX)
-		// these platforms don't do shm_open as defined in the spec 
-		char* base = strrchr(fileName,'/');
-		assert(base);
-		strcpy(fileName,base);
-	#endif
-	fprintf2((stderr,"calling shm_open with \"%s\"\n",fileName));
-	int fd = shm_open(fileName,O_RDWR|O_CREAT|O_EXCL,0600);
-	if (fd < 0) {
-		fprintf(stderr, "shm_open \"%s\": ", fileName);
-		perror(0);
-	}
+    unsigned i = 0;
+    while (true) {
+        sprintf(fileName, "/MirrorBufferSet-%u", i);
+        #if defined(OS_IRIX) || defined(OS_HPUX)
+        // these platforms don't do shm_open as defined in the spec 
+        tmpnam(fileName);
+        fprintf2((stderr,"tmpnam returned \"%s\"\n",fileName));
+        #endif
+        fprintf2((stderr,"calling shm_open with \"%s\"\n",fileName));
+        int fd = shm_open(fileName,O_RDWR|O_CREAT|O_EXCL,0600);
+        if (fd < 0) {
+            if (errno == EEXIST) {
+                ++i;
+            } else{
+                fprintf(stderr, "shm_open \"%s\": ", fileName);
+                perror(0);
+                return -1;
+            }
+        } else {
+            // remove the shared memory object
+            if ( shm_unlink(fileName) ) {
+                fprintf(stderr, "### shm_unlink(\"%s\") failed: ", fileName);
+                perror(0);
+            }
+            return fd;
+        }
+    }
 #else
-	int fd = open(fileName, O_RDWR|O_CREAT, 0666);
-	if(fd < 0) {
-		fprintf(stderr, "### Error opening file \"%s\": ", fileName);
-		perror(0);
-	}
+    sprintf(fileName, "/tmp/MirrorBufferSet-XXXXXX");
+    int fd = mkstemp(fileName);
+    // remove the temporary file
+    if ( unlink(fileName) ) {
+        fprintf(stderr, "### unlink(\"%s\") failed: ", fileName);
+        perror(0);
+    }
+    return fd;
 #endif
-	return fd;
 }
 
 
@@ -220,30 +234,16 @@ int MirrorBufferSet::GetFileDescriptor(void)
 MirrorBufferSet::~MirrorBufferSet(void)
 //-----------------------------------------------------------------------------
 {
-	if (!bufferBase) return;
+    if (!bufferBase) return;
 
-	// unmap the file
-	if ( munmap((caddr_t)bufferBase, (bufferSize+mirrorSize)*numBuffers) ) {
-		perror("munmap");
-	}
-	fprintf2((stderr,"unmapped- \"%s\" [0x%08X - 0x%08X), %d bytes\n",
-		fileName, bufferBase,
+    // unmap the file
+    if ( munmap((caddr_t)bufferBase, (bufferSize+mirrorSize)*numBuffers) ) {
+        perror("munmap");
+    }
+    fprintf2((stderr,"unmapped- \"%s\" [0x%08X - 0x%08X), %d bytes\n",
+        fileName, bufferBase,
         (char*)bufferBase+((bufferSize+mirrorSize)*numBuffers),
         ((bufferSize+mirrorSize)*numBuffers)));
-	
-#if MBS_USE_POSIX_SHM
-	// remove the shared memory object
-	if ( shm_unlink(fileName) ) {
-		fprintf(stderr, "### shm_unlink(\"%s\") failed: ", fileName);
-		perror(0);
-	}
-#else
-	// remove the temporary file
-	if ( unlink(fileName) ) {
-		fprintf(stderr, "### unlink(\"%s\") failed: ", fileName);
-		perror(0);
-	}
-#endif
 }
 
 
@@ -252,9 +252,9 @@ unsigned long MirrorBufferSet::PageSize(void)
 //-----------------------------------------------------------------------------
 {
 #ifdef OS_DARWIN
-	return getpagesize();
+    return getpagesize();
 #else
-	return sysconf(_SC_PAGESIZE);
+    return sysconf(_SC_PAGESIZE);
 #endif
 }
 
