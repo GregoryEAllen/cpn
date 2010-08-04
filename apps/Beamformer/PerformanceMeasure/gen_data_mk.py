9b816f7d94e46e2088832d160ccc4a916d3a9f84
John Bridgman <johnfb@arlut.utexas.edu>
2010-08-04 14:01:37 -0500

    Added a third type of test...

--- apps/Beamformer/PerformanceMeasure/gen_data_mk.py
+++ apps/Beamformer/PerformanceMeasure/gen_data_mk.py
@@ -1,25 +1,26 @@
 
 import sys
 
+wait_policies = [('passive', 'make_data'), ('active', 'make_data'), ('10000', 'make_data_spin')]
 stdout = sys.stdout
 for i in range(1,17):
-    for wait_policy in ['passive', 'active']:
+    for wait_policy in wait_policies:
         for oper in ['vbf', 'hbf', 'ompbf', 'cpnbf']:
-            file = oper + '.' + str(i) + '.' + wait_policy
+            file = oper + '.' + str(i) + '.' + wait_policy[0]
             stdout.write('data_files += ' + file + '\n')
             stdout.write(file + ':\n')
-            stdout.write('\t$(call make_data,' + oper.upper()+ ',' + wait_policy + ',' + str(i) + ',$@)\n\n')
+            stdout.write('\t$(call ' + wait_policy[1] + ',' + oper.upper()+ ',' + wait_policy[0] + ',' + str(i) + ',$@)\n\n')
 
 
 for action_type in ['avg', 'min', 'max']:
-    for wait_policy in ['passive', 'active']:
+    for wait_policy in wait_policies:
         for operation_type in ['vbf', 'hbf', 'ompbf', 'cpnbf']:
-            file = operation_type + '.' + action_type + '.' + wait_policy
+            file = operation_type + '.' + action_type + '.' + wait_policy[0]
             stdout.write(operation_type + '_' + action_type + '_files += ' + file + '\n')
             stdout.write('intermediate_files += ' + file + '\n')
             stdout.write(file + ':')
             for i in range(1,17):
-                stdout.write(' ' + operation_type + '.' + str(i) + '.' + wait_policy)
+                stdout.write(' ' + operation_type + '.' + str(i) + '.' + wait_policy[0])
             stdout.write('\n')
             stdout.write('\t$(call compute_' + action_type + ',$^,$@)\n\n')
 
