048990fa1ffa41c6d7dae52bc7bbfa6a3b14af25
John Bridgman <johnfb@arlut.utexas.edu>
2010-09-08 12:00:52 -0500

    Added license and simple documentation to the files in the node library.

--- nodelibraries/JoinNode/JoinNode.h
+++ nodelibraries/JoinNode/JoinNode.h
@@ -1,4 +1,29 @@
-
+//=============================================================================
+//	Computational Process Networks class library
+//	Copyright (C) 1997-2006  Gregory E. Allen and The University of Texas
+//
+//	This library is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Library General Public License as published
+//	by the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This library is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//	Library General Public License for more details.
+//
+//	The GNU Public License is available in the file LICENSE, or you
+//	can write to the Free Software Foundation, Inc., 59 Temple Place -
+//	Suite 330, Boston, MA 02111-1307, USA, or you can find it on the
+//	World Wide Web at http://www.fsf.org.
+//=============================================================================
+/** \file
+ * \author John Bridgman
+ * The JoinNode reads from the inputs in a round robbin fashion and
+ * then writes to the output, optionally discarding some portion read.
+ */
+#ifndef JOINNODE_H
+#define JOINNODE_H
 #pragma once
 
 #include "NodeBase.h"
@@ -17,4 +42,4 @@ private:
     unsigned size;
     unsigned overlap;
 };
-
+#endif
