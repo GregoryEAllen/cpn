27d1e51a40e2475ecb32d8ac287f6652bcd7aef4
John Bridgman <johnfb@arlut.utexas.edu>
2011-04-01 10:58:26 -0500

    Updated the queue test to behave like the linktest.

--- tools/queuetest/main.cc
+++ tools/queuetest/main.cc
@@ -3,14 +3,18 @@
 #include <sys/time.h>
 #include <iostream>
 #include <string>
+#include <limits>
 #include <iomanip>
+#include <errno.h>
 #include "Kernel.h"
+#include "ParseBool.h"
 #include "NodeBase.h"
 #include "ErrnoException.h"
 #include "IQueue.h"
 #include "OQueue.h"
 #include "RemoteContext.h"
 #include "SocketAddress.h"
+#include "Filter.h"
 
 using namespace std;
 using namespace CPN;
@@ -22,6 +26,20 @@ static double getTime() {
     }
     return static_cast<double>(tv.tv_sec) + 1e-6 * static_cast<double>(tv.tv_usec);
 }
+
+static void Sleep(double t) {
+    timespec tsrec, tsrem;
+    tsrec.tv_sec = (time_t)floor(t);
+    tsrec.tv_nsec = (long)((t - floor(t))*1e9);
+    while (nanosleep(&tsrec, &tsrem) != 0) {
+        if (errno == EINTR) {
+            tsrec = tsrem;
+        } else {
+            throw ErrnoException();
+        }
+    }
+}
+
 struct Bytes {
     Bytes(double v_) : v(v_) {}
     double v;
@@ -56,31 +74,58 @@ std::ostream &operator<<(std::ostream &os, const Time &t) {
     return os;
 }
 
-void Sender(NodeBase *node, int buffer_len, double report_time) {
+const double a[] = { 1, -1.88903307939452, 0.894874344616636 };
+const double b[] = { 0.00146031630552773, 0.00292063261105546, 0.00146031630552773 };
+const unsigned filt_len = 3;
+
+void Sender(NodeBase *node, int buffer_len, double report_time, double rate_throttle) {
     cout << "Sender start" << endl;
+    std::vector<double> filt_buf(filt_len, 0);
     OQueue<char> out = node->GetOQueue("out");
+    double min_rate = std::numeric_limits<double>::infinity();
+    double max_rate = 0;
     uint64_t total_bytes = 0;
     double total_time = 0;
     while (true) {
         double start = getTime();
+        double cur = getTime();
         uint64_t numsent = 0;
-        while (getTime() - start < report_time) {
+        while (cur - start < report_time) {
+            if (rate_throttle > 0 && numsent > 0) {
+                double min_time = numsent/rate_throttle;
+                double dur = cur - start;
+                if (min_time > dur) {
+                    Sleep(min_time - dur);
+                }
+            }
             out.GetEnqueuePtr(buffer_len);
             out.Enqueue(buffer_len);
             numsent += buffer_len;
+            cur = getTime();
         }
-        double end = getTime();
+        double end = cur;
         double dur = end - start;
         total_bytes += numsent;
         total_time += dur;
-        cout << "Sent " << Bytes(numsent) << " in " << Time(dur) << " at " <<
-            Bytes(numsent/dur) << "/sec; Avg: " << Bytes(total_bytes/total_time) << "/sec" << endl;
+        double rate = numsent/dur;
+        min_rate = min(rate, min_rate);
+        max_rate = max(rate, max_rate);
+        double lp_rate = Filter(rate, b, filt_len, a, filt_len, &filt_buf[0]);
+        cout << "Sent "
+            << Bytes(numsent) << " in " << Time(dur) << " at " << Bytes(rate)
+            << "/sec;\n" << "Min: " << Bytes(min_rate) << "/s Max: " << Bytes(max_rate)
+            << "/s Avg: " << Bytes(total_bytes/total_time) << "/s"
+            << " LP: " << Bytes(lp_rate) << "/s"
+            << endl;
     }
 }
 
 void Receiver(NodeBase *node, int buffer_len, double report_time) {
     cout << "Receiver start" << endl;
+    std::vector<double> filt_buf(filt_len, 0);
     IQueue<char> in = node->GetIQueue("in");
+    double min_rate = std::numeric_limits<double>::infinity();
+    double max_rate = 0;
     uint64_t total_bytes = 0;
     double total_time = 0;
     while (true) {
@@ -98,8 +143,16 @@ void Receiver(NodeBase *node, int buffer_len, double report_time) {
         double dur = end - start;
         total_bytes += numread;
         total_time += dur;
-        cout << "Recv " << Bytes(numread) << " in " << Time(dur) << " at " <<
-            Bytes(numread/dur) << "/sec; Avg: " << Bytes(total_bytes/total_time) << "/sec" << endl;
+        double rate = numread/dur;
+        min_rate = min(rate, min_rate);
+        max_rate = max(rate, max_rate);
+        double lp_rate = Filter(rate, b, filt_len, a, filt_len, &filt_buf[0]);
+        cout << "Recv "
+            << Bytes(numread) << " in " << Time(dur) << " at " << Bytes(rate)
+            << "/sec;\n" << "Min: " << Bytes(min_rate) << "/s Max: " << Bytes(max_rate)
+            << "/s Avg: " << Bytes(total_bytes/total_time) << "/s"
+            << " LP: " << Bytes(lp_rate) << "/s"
+            << endl;
     }
 }
 
@@ -111,10 +164,11 @@ int main(int argc, char **argv) {
     bool sender = false;
     int queue_size = 0;
     int threshold_size = 0;
-    bool threshold = false;
+    bool threshold = true;
     double report_time = 0.1;
+    double rate_throttle = -1;
     while (true) {
-        int c = getopt(argc, argv, "h:p:sb:q:t:lTr:");
+        int c = getopt(argc, argv, "h:p:sb:q:t:lTr:Hm:");
         if (c == -1) break;
         switch (c) {
         case 'h':
@@ -139,23 +193,43 @@ int main(int argc, char **argv) {
             threshold_size = atoi(optarg);
             break;
         case 'T':
-            threshold = true;
+            threshold = ParseBool(optarg);
             break;
         case 'r':
             report_time = strtod(optarg, 0);
             break;
-        default:
+        case 'm':
+            rate_throttle = strtod(optarg, 0);
             break;
+        case 'H':
+        default:
+            cerr << "Usage: " << *argv << " [options] <context host> <context port>\n"
+                "       " << *argv << " -l [options]\n"
+                "\t-h n\t kernel hostname\n"
+                "\t-p n\t kernel port name\n"
+                "\t-s  \t Act as sender\n"
+                "\t-b n\t Specify buffer size (current: " << buffer_len << ")\n"
+                "\t-l  \t Run locally.\n"
+                "\t-q n\t Specify queue size (current: " << queue_size << ")\n"
+                "\t-t n\t Specify threshold size (current " << threshold_size << ")\n"
+                "\t-Tyn\t Specify whether to use the vmm queue (current: " << threshold << ")\n"
+                "\t-r n\t Time in seconds between reports (current: " << report_time << ")\n"
+                "\t-m n\t Maximum speed in bytes per second to throttle to (<=0 no throttle) (current: "
+                << rate_throttle << ")\n"
+                ;
+            return 0;
         }
     }
 
     if (local) {
         Kernel kernel("kernel");
-        kernel.CreateFunctionNode("send", Sender, buffer_len, report_time);
+        kernel.CreateFunctionNode("send", Sender, buffer_len, report_time, rate_throttle);
         kernel.CreateFunctionNode("recv", Receiver, buffer_len, report_time);
         QueueAttr qattr( (queue_size > 0 ? queue_size : 2*buffer_len), (threshold_size > 0 ? threshold_size : buffer_len) );
         qattr.SetWriter("send", "out").SetReader("recv", "in");
-        qattr.SetHint(QUEUEHINT_THRESHOLD);
+        if (threshold) {
+            qattr.SetHint(QUEUEHINT_THRESHOLD);
+        }
         kernel.CreateQueue(qattr);
         kernel.Wait();
     } else {
@@ -167,10 +241,12 @@ int main(int argc, char **argv) {
         context.reset(new RemoteContext(SocketAddress::CreateIP(argv[optind], argv[optind+1])));
         if (sender) {
             Kernel kernel(KernelAttr("send").SetContext(context).SetHostName(kernelhost).SetServName(kernelport).UseD4R(false));
-            kernel.CreateFunctionNode("send", Sender, buffer_len, report_time);
+            kernel.CreateFunctionNode("send", Sender, buffer_len, report_time, rate_throttle);
             QueueAttr qattr( max(queue_size, 2*buffer_len), max(threshold_size, buffer_len) );
             qattr.SetWriter("send", "out").SetReader("recv", "in");
-            qattr.SetHint(QUEUEHINT_THRESHOLD);
+            if (threshold) {
+                qattr.SetHint(QUEUEHINT_THRESHOLD);
+            }
             kernel.CreateQueue(qattr);
             kernel.Wait();
         } else {
